4e7a4634735f034670ab304b8bddd65371033e3f
rkeaveney <ronan.keaveney@gmail.com>
2023-11-11 14:32:03 +0000

    Added EJS skeleton

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+import bodyParser from "body-parser";
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 8000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req,res) => {
-    res.send("<h1>Hello World!</h1>");
+    res.render(__dirname + "/public/index.ejs");
+});
+
+app.post("/detect", (req,res) => {
+    console.log(req.body);
+    res.render(__dirname + "/public/result.ejs", { "body": req.body });
 });
 
 app.listen(port, () => {
